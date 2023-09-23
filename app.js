bae30fb6e1893b2f6fe4fc27654ce679efd72d51
VAISHNAVI KAWALE <114976538+vais2004@users.noreply.github.com>
2023-09-23 21:21:44 +0530

    update

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-var readlineSync = require('readline-sync');
-let highestMarks = 0;
-let name = '';
 
-for(let i=0; i <5; i=i+1) {
-  let studentNames = readlineSync.question("Enter Student no. " + [i+1] + " name: ")
-  let unitTestMarks = Number(readlineSync.question("Unit test marks: "));
-  let preFinalMarks = Number(readlineSync.question("Pre Final marks: "));
-  let finalMarks = Number(readlineSync.question("Final marks: "))
-  let totalMarks = unitTestMarks + preFinalMarks + finalMarks;
-  let averageMarks = totalMarks / 3;
-  console.log("-> Total Marks: " + totalMarks);
-  console.log("-> Average Marks: " + averageMarks.toFixed(2));
-  console.log("===================")
+const readline = require('readline-sync')
 
-  if(totalMarks > highestMarks) {
-    highestMarks = totalMarks;
-    name = studentNames;
+const data = [];
+let maximumMarks = 0;
+let firstRank;
+
+for (let i = 0; i < 5; i++) {
+  const userName = readline.question("what's ypur name ");
+  const unitTestMarks = Number(readline.question("Enter your unit marks "));
+  const preFinalMarks = Number(readline.question("Enter your pre final marks "));
+  const finalMarks = Number(readline.question("Enter your final marks "));
+  const totalMarks = unitTestMarks + preFinalMarks + finalMarks;
+  console.log("------------")
+  
+  data.push({ userName, unitTestMarks, preFinalMarks, finalMarks, totalMarks });
+
+}
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i].totalMarks > maximumMarks) {
+    maximumMarks = data[i].totalMarks;
+    firstRank = data[i].userName;
   }
 }
 
-console.log("Congratulations!"+"\n" + name + " got the highest marks, which is " + highestMarks)
+let average = 0;
+for (let i = 0; i < data.length; i++) {
+  average += data[i].totalMarks;
+}
+average /= data.length;
+
+console.log(firstRank + " has git the first Rank and Highest total mark is : " + maximumMarks + " and the average marks is " + average);
